d7a42a624a29c3de53dac97f739297895a52ce4c
fkm-X3 <fkmbloodshead@gmail.com>
2025-11-22 23:40:23 +1100

    Fixed color problem

---
+++ Javascript_folder/Music_Fix.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        darkBg: '#121212',   
+                        darkCard: '#181818',
+                    }
+                }
+            }
+        }
